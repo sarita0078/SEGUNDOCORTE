749368e7abe00acc69d18dd8eb0349d4533c4c3e
Sara <snvillamizar37@ucatolica.edu.co>
2026-03-20 11:44:26 -0500

    commit clase 20/03/2026

--- main.py
+++ main.py
@@ -2,10 +2,39 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def mensaje():
-    return "Bienvenidos a FastAPI ingenieros de sistemas"
+productos = [
+    {
+        "codigo" : 1,
+        "nombre" : "Esfero",
+        "valor" : 3500,
+        "existencia" : 10
+    },
+    {
+        "codigo" : 2,
+        "nombre" : "Cuaderno",
+        "valor" : 5000,
+        "existencia" : 15
+    },
+    {
+        "codigo" : 3,
+        "nombre" : "Lapiz",
+        "valor" : 200,
+        "existencia" : 12
+    }
+]
 
-@app.get("/{nombre}")
-def mensaje(nombre: str):
-    return f"Bienvenidos {nombre}"
+@app.get('/')
+def mensaje():
+    return "Bienvenido a la tienda de papeleria"
+
+@app.get('/{nombre}/{codigo}')
+def mensaje2(nombre: str, codigo: int):
+    return f"Bienvenido a la tienda de papeleria, {nombre}!"
+
+@app.get('/uno')
+def mensaje3(nombre: str, edad: int):
+    return f"Bienvenido a la tienda de papeleria, {nombre}! Tu edad es {edad} años."
+
+@app.get('/productos')
+def obtener_productos():
+    return productos
