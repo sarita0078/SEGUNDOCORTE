652cbe0bbf524f9dcf62b6864578b2ed53707bf8
Sara <snvillamizar37@ucatolica.edu.co>
2026-03-20 12:43:29 -0500

    commit FASTAPI clase 20/03/2026

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -18,7 +18,7 @@ productos = [
     {
         "codigo" : 3,
         "nombre" : "Lapiz",
-        "valor" : 200,
+        "valor" : 2000,
         "existencia" : 12
     }
 ]
@@ -35,6 +35,70 @@ def mensaje2(nombre: str, codigo: int):
 def mensaje3(nombre: str, edad: int):
     return f"Bienvenido a la tienda de papeleria, {nombre}! Tu edad es {edad} años."
 
-@app.get('/productos')
-def obtener_productos():
+@app.get('/productos/{codigo}')
+def listadeproductos():
     return productos
+
+@app.get('/productos/{codigo}')
+def obtener_producto(codigo: int):
+    for producto in productos:
+        if producto["codigo"] == codigo:
+            return producto
+    return {"mensaje": "Producto no encontrado"}
+
+@app.get('/productos/{nombre}')
+def obtener_producto_por_nombre(nombre: str):
+    for producto in productos:
+        if producto["nombre"] == nombre:
+            return producto
+    return {"mensaje": "Producto no encontrado"}
+
+@app.post('/productos')
+def crearProductos(
+    codigo: int = Body(), 
+    nombre: str = Body(), 
+    valor: float = Body(), 
+    existencia: int = Body()):
+    productos.append({
+        'codigo': codigo,
+        'nombre': nombre,
+        'valor': valor,
+        'existencia': existencia
+    })
+    return productos
+
+@app.post('/productos2')
+def crearProductos2(
+    codigo:int=Body(), 
+    nombre:str=Body(), 
+    valor:float=Body(), 
+    existencia:int=Body()):
+    productos.append({
+        'codigo': codigo,
+        'nombre': nombre,
+        'valor': valor,
+        'existencia': existencia
+    }
+    )
+    return productos
+
+@app.put('/productos/{codigo}')
+def actualizar_producto(codigo: int, 
+    nombre: str = Body(), 
+    valor: float = Body(),
+    existencia: int = Body()):
+    for producto in productos:
+        if producto['codigo'] == codigo:
+            producto['nombre'] = nombre
+            producto['valor'] = valor
+            producto['existencia'] = existencia
+            return producto
+    return {"mensaje": "Producto no encontrado"}
+
+@app.delete('/productos/{codigo}')
+def eliminar_producto(codigo: int):
+    for producto in productos:
+        if producto['codigo'] == codigo:
+            productos.remove(producto)
+            return {"mensaje": "Producto eliminado"}
+    return {"mensaje": "Producto no encontrado"}
